87df986f9a461fa22d1808584b652492f811f6da
Talismar <talismar788.una@gmail.com>
2023-04-28 14:24:39 -0300

    Adding info_list.py file

---
+++ info_list.py
@@ -0,0 +1,9 @@
+def get_info_list(studies_list: str) -> str:
+    ret = """
+            <h1>Pipes and Filters</h1>
+            <h3>Turma: ADS</h3>
+            <h3>Ano: 2021</h3>
+            <h3>Alunos: </h3>
+          """
+
+    return ret + studies_list
